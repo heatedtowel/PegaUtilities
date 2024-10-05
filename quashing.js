ea00f3dc27b58030bc0dc02c7bd569a8ed14a2de
Jesse Monks <heateddiv@gmail.com>
2024-10-05 13:19:00 -0400

    adding name variable

--- quashing.js
+++ quashing.js
@@ -16,7 +16,7 @@ javascript:(function () {
     const displayRegionSelect = () => {
         let regionOptions = ['NCSA', 'EMEA', 'APAC'];
         let currentRegion = getLocalStorage('region');
-
+        let currentName = getLocalStorage('name');
 
         let overlay = document.createElement('div');
         overlay.className = 'bookMarklet';
@@ -70,6 +70,26 @@ javascript:(function () {
         dropdownContainer.appendChild(dropdownLabel);
         dropdownContainer.appendChild(selectElement);
 
+        let nameInput = document.createElement('input');
+        nameInput.id = 'engineerName';
+        nameInput.type = 'text';
+        nameInput.placeholder = !currentName ? 'Enter Name' : currentName;
+
+        let saveName = document.createElement('button');
+        saveName.textContent = 'Save';
+        saveName.disabled = currentName;
+        saveName.style.padding = '2px 8px';
+        saveName.style.borderRadius = '1rem';
+        saveName.style.backgroundColor = 'grey';
+        saveName.style.cursor = 'pointer';
+
+        saveName.addEventListener('click' , () => {
+            if (!currentName) {
+                let newName = document.getElementById('engineerName').value;
+                setLocalStorage('name', newName);
+            }
+        });
+
         let saveRegion = document.createElement('button');
         saveRegion.textContent = 'Save';
         saveRegion.disabled = currentRegion;
@@ -93,7 +113,7 @@ javascript:(function () {
 
         nextButton.addEventListener('click' , () => {
             overlay.remove();
-            displayEnoughInformationToTriage();
+            displayEnoughInformationToTriage(currentRegion, currentName);
         });
 
         btnContainer.appendChild(saveRegion);
@@ -101,6 +121,8 @@ javascript:(function () {
 
         overlay.appendChild(title);
         overlay.appendChild(dropdownContainer);
+        overlay.appendChild(nameInput);
+        overlay.appendChild(saveName);
         overlay.appendChild(btnContainer);
         document.body.appendChild(overlay);
     };
@@ -119,7 +141,7 @@ javascript:(function () {
         return localItem;
     };
 
-    const displayEnoughInformationToTriage = () => {
+    const displayEnoughInformationToTriage = (region, name) => {
         const informationOptions = ['Yes', 'No'];
 
         let overlay = document.createElement('div');
@@ -179,12 +201,17 @@ javascript:(function () {
             const selection = document.getElementById('informationToTriage').value;
 
             if (selection === 'Yes') {
-                displayTemplate();
+                displayTemplate(region, name);
             }
         })
     };
 
-    const displayTemplate = () => {
+    const displayTemplate = (region, name) => {
+        const quashTemplate = {
+            id: 'quashTemplate',
+            responseNo: `Thank you for reaching out to Pega GCS Support, we are currently routing your request to the appropriate engineer for this request. You will be notified once an engineer has been assigned. Regards, ${name}`
+        };
+
         let overlay = document.createElement('div');
         overlay.className = 'bookMarklet';
         overlay.style.position = 'fixed';
@@ -206,8 +233,8 @@ javascript:(function () {
         title.style.color = 'black';
 
         let template = document.createElement('p');
-        template.id  = 'quashTemplate';
-        template.textContent = 'testing template';
+        template.id  = 'quashTemplate.id';
+        template.textContent = quashTemplate.responseNo;
         template.style.color = 'black';
 
         let copyBtn = document.createElement('button');
